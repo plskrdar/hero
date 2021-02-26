5823d70a7f29c06cec737177f7463cedbbfa4ae7
plskrdar <dariusz.skrzypek87@gmail.com>
2021-02-26 23:56:05 +0100

    implemented routing and dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import {AppComponent} from './app.component';
 import {HeroesComponent} from './heroes/heroes.component';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 import {MessagesComponent} from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -13,11 +16,13 @@ import {MessagesComponent} from './messages/messages.component';
         AppComponent,
         HeroesComponent,
         HeroDetailComponent,
-        MessagesComponent
+        MessagesComponent,
+        DashboardComponent
     ],
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        AppRoutingModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
